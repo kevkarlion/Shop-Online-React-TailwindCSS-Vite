2b2ecf7188b56ab9e55108d6a411eb013e02d9a4
Kevin Riquelme <kriquelme10@gmail.com>
2023-10-30 22:10:33 -0300

    Inicio de proyecto con React-Vite-TailwindCSS

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='bg-emerald-800	'>
+        Hola mundo!
+      </div>
+      
+    </>
+  )
+}
+
+export default App
