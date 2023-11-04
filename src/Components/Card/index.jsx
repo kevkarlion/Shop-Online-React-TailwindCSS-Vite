ea3b79c11439dac841e2d43fe5ef75d26b10fc85
Kevin Riquelme <kriquelme10@gmail.com>
2023-11-04 00:40:54 -0300

    bg-white al navbar

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -2,30 +2,37 @@
 import useApi from "../../API";
 
 const Card = () => {
+  //Custom hook Api
+  const items = useApi();
 
-    //Custom hook Api
-    const items = useApi();
-
-    return (
-        <div className="w-56 h-60">
-            {items.map((item) => (
-                <div key={item.id} className="bg-white cursor-pointer w-56 h-60 rounded-lg">
-                    <figure className="relative mb-4 w-full h-4/5">
-                        <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg  text-black text-xs m-2 px-3 py-0.5">{item.category}</span>
-                        <img className="w-full h-full object-cover rounded-lg" src={item.image} alt="Headphones" />
-                        <div className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1">
-                            +
-                        </div>
-                    </figure>
-                    <p className="flex justify-between">
-                        <span className="text-sm font-light">{item.title}</span>
-                        <span className="text-lg font-medium">{item.price}</span>
-                    </p>
-                </div>
-            ))}
+  return (
+    <div className="grid gap-4 grid-cols-3 ">
+      {items.map((item) => (
+        <div
+          key={item.id}
+          className="bg-white cursor-pointer w-40 h-44 rounded-lg"
+        >
+          <figure className="relative mb-4 w-full h-4/5">
+            <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg  text-black text-xs m-2 px-3 py-0.5">
+              {item.category}
+            </span>
+            <img
+              className="w-full h-full object-cover rounded-lg"
+              src={item.image}
+              alt="Headphones"
+            />
+            <div className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1">
+              +
+            </div>
+          </figure>
+          <p className="flex justify-between">
+            <span className="text-sm font-light">{item.title}</span>
+            <span className="text-lg font-medium">{item.price}</span>
+          </p>
         </div>
-       
-    )
-}
+      ))}
+    </div>
+  );
+};
 
 export default Card;
