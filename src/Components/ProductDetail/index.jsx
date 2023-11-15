acf169d77aeba34b965fd43ed26813e5641dcbe0
Kevin Riquelme <kriquelme10@gmail.com>
2023-11-15 00:36:32 -0300

    css a productDetail

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -14,7 +14,7 @@ export const ProductDetail = () => {
       <nav className="side-nav">
         <span>My Order</span>
         {context.items.map((item) => (
-          <div key={item.id} className="flex mt-8 m-2 justify-center items-center w-full">
+          <div key={item.id} className="flex mt-8 p-2 justify-center items-center w-full">
             <div className="flex items-center justify-center ml-2">
               <figure className="w-16 h-18  mr-2">
                 <img
@@ -23,10 +23,10 @@ export const ProductDetail = () => {
                   className="object-cover w-full h-full"
                 />
               </figure>
-              <p className="text-xs pl-2 mr-4">{item.title}</p>
+              <p className="text-xs pl-2 mr-4 w-28">{item.title}</p>
             </div>
             <p className="text-sm  font-bold mr-2">${item.price}</p>
-            <span className="mr-4 cursor-pointer" onClick={() => itemDeleted(item)}>
+            <span className="mr-4 cursor-pointer " onClick={() => itemDeleted(item)}>
               x
             </span>
           </div>
