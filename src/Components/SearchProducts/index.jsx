a66cdc82b7ae24500469e9c783aa610a86e09ece
Kevin Riquelme <kriquelme10@gmail.com>
2023-11-27 23:01:20 -0300

    search enhance

--- src/Components/SearchProducts/index.jsx
+++ src/Components/SearchProducts/index.jsx
@@ -4,21 +4,19 @@ import { MiContext } from "../Context";
 export function SearchProducts() {
   const context = useContext(MiContext);
 
-  const productFind = (e)=> {
+  const productFind = (e) => {
     context.setFind(e.target.value);
-  }
-
-
+  };
 
   return (
     <div>
       <input
-        onChange={(e)=> productFind(e)}
+        onChange={(e) => productFind(e)}
+        value={context.find}
         className=" w-80 h-8 pl-2 m-3 mb-6 rounded-md border-black border-solid border-[1px] outline-0"
         placeholder="Search"
         type="text"
       />
-      
     </div>
   );
 }
