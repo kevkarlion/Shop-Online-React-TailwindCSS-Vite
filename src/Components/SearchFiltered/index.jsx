e109a86816bcf4aaca118470a9d78de85a601f29
Kevin Riquelme <kriquelme10@gmail.com>
2023-12-02 22:21:33 -0300

    custom hook del SearchFiltered

--- src/Components/SearchFiltered/index.jsx
+++ src/Components/SearchFiltered/index.jsx
@@ -1,58 +1,11 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import { MiContext } from "../Context";
-import { useContext, useState, useEffect } from "react";
-import useApi from "../../API";
+import { useFiltered } from '../useFiltered';
 import "./styles.css";
 
 export function SearchFiltered() {
-  //Custom hook Api
-  const items = useApi();
 
 
-  const [filtered, setFiltered] = useState('');
-
-  const context = useContext(MiContext);
-
-  //Agrega un item al carrito y abre el detalle del pedido
-  function addToCart(product) {
-    if (context.door === false) {
-      context.toggleProductDetail();
-    }
-    context.increaseCountCart();
-    context.addProduct(product);
-  }
-
-
-  //Effect para filtrar por categoria
-  useEffect(() => {
-    const pathSelect = window.location.pathname.slice(1);
-    if (pathSelect === `clothes`){
-      setFiltered(`men's clothing`); 
-    }
-    if (pathSelect === 'jewelery' || pathSelect === 'electronics'){
-      setFiltered(pathSelect);
-    }
-    if (pathSelect === '' ){
-      setFiltered(true);
-    }
-  }, [window.location.pathname]);
-
-
-  
-  //Search - Metodo para buscar por categoria segun el path
-  const itemsFiltered = items.filter(
-    (product) => product.category === filtered || !!product.category === filtered
-  );
-
-  
-
-  //Search
-  const valueFind = context.find;
-
-  //Search personalizada del input
-  const productFiltered = itemsFiltered.filter((product) =>
-    product.title.toLowerCase().includes(valueFind.toLowerCase())
-  );
+  //custom hook useFilteder
+  const {addToCart, productFiltered } = useFiltered();
 
   
   return (
---
+++ src/Components/useFiltered/index.jsx
@@ -0,0 +1,56 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+
+import { useContext } from "react";
+import { MiContext } from "../Context";
+import { useEffect, useState } from "react";
+import useApi from "../../API";
+
+
+
+//hook personalizado de SearchFiltered
+export function useFiltered(){
+
+    const items = useApi();
+    const context = useContext(MiContext);
+    
+    //Agrega un item al carrito y abre el detalle del pedido
+    function addToCart (product) {
+        if (context.door === false) {
+        context.toggleProductDetail();
+        }
+        context.increaseCountCart();
+        context.addProduct(product);
+    }
+
+
+    const [filtered, setFiltered] = useState('');
+    //Effect para filtrar por categoria
+    useEffect(() => {
+        const pathSelect = window.location.pathname.slice(1);
+        if (pathSelect === `clothes`){
+        setFiltered(`men's clothing`); 
+        }
+        if (pathSelect === 'jewelery' || pathSelect === 'electronics'){
+        setFiltered(pathSelect);
+        }
+        if (pathSelect === '' ){
+        setFiltered(true);
+        }
+    }, [window.location.pathname]);
+
+    //Search - Metodo para buscar por categoria segun el path
+    const itemsFiltered = items.filter(
+        (product) => product.category === filtered || !!product.category === filtered
+    );
+
+    //Search - input del usuario
+    const valueFind = context.find;
+
+    //Search filtrada segun el input
+    const productFiltered = itemsFiltered.filter((product) =>
+        product.title.toLowerCase().includes(valueFind.toLowerCase())
+    );
+
+    return { addToCart, productFiltered };
+   
+}
