4ef418185a3c7ee95179f8ed03bad9a69421d5de
Kevin Riquelme <kriquelme10@gmail.com>
2023-12-07 22:58:31 -0300

    CSS a SearchFiltered

--- src/Components/Layout/index.jsx
+++ src/Components/Layout/index.jsx
@@ -8,7 +8,7 @@
 */
 const Layout = ( { children }) => {
     return (
-        <div className="flex flex-col items-center mt-20">
+        <div className="flex flex-col items-center mt-20 bg-stone-100	">
             { children }
         </div>
     );
--- src/Components/SearchFiltered/index.jsx
+++ src/Components/SearchFiltered/index.jsx
@@ -1,4 +1,6 @@
 import { useFiltered } from "../useFiltered";
+import { TfiMoney } from "react-icons/tfi";
+
 import "./styles.css";
 
 export function SearchFiltered() {
@@ -8,22 +10,21 @@ export function SearchFiltered() {
   return (
     <div className="container-grid">
       {productFiltered.map((item) => (
-        <div key={item.id} className="bg-white h-auto rounded-lg">
-          <figure className="relative  w-full h-auto">
+        <div key={item.id} className="flex flex-col bg-white h-full rounded-lg border-solid border-[1px]  w-[130px] duration-300 hover:shadow-2xl group transform transition-transform hover:scale-105 ease-in-out overflow-hidden">
+          <figure className="relative  w-full h-full border-b p-2	transform group-hover:scale-105 transition-transform">
             <span
-              className="absolute bottom-0 left-0 bg-white/60 rounded-lg  
-                    text-black text-xs mb-6 ml-2 px-3 py-0.5"
+              className="transform transition-transform group-hover:scale-105 absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs mb-6 ml-2 px-3 py-0.5"
             >
               {item.category}
             </span>
             <img
-              className="w-full h-full object-cover rounded-lg"
+              className="transform group-hover:scale-105 transition-transform w-full h-full object-contain rounded-lg pb-2"
               src={item.image}
               alt="Headphones"
             />
             <div
-              className="absolute top-0 right-0 flex justify-center items-center 
-                        bg-white w-6  h-6 rounded-full border-[1px] border-black  m-2 p-1 cursor-pointer"
+              className="transform group-hover:scale-105 transition-transform absolute top-0 right-0 flex justify-center items-center 
+                        bg-white w-6  h-6 rounded-full border-[1px] border-black  m-2 p-1 cursor-pointer "
               onClick={() => addToCart(item)}
             >
               <svg
@@ -32,7 +33,7 @@ export function SearchFiltered() {
                 viewBox="0 0 24 24"
                 strokeWidth={1.5}
                 stroke="currentColor"
-                className="w-6 h-6"
+                className="transform group-hover:scale-105 transition-transform w-6 h-6"
               >
                 <path
                   strokeLinecap="round"
@@ -42,11 +43,12 @@ export function SearchFiltered() {
               </svg>
             </div>
           </figure>
-          <p className="flex justify-between h-auto mb-1 ">
-            <span className="text-xs font-light">{item.title}</span>
-            <span className="text-sm font-medium">{item.price}</span>
+          <p className=" flex flex-col justify-center items-center h-full  mt-4 rounded-b-lg">
+            <span className=" flex items-center  justify-center text-xs font-light mb-4 p-2">{item.title}</span>
+            <span className="flex justify-center items-center text-sm font-medium mt-auto w-full bg-black text-white rounded-b-lg transform group-hover:scale-105 transition-transform"> <TfiMoney /> {item.price}</span>
           </p>
         </div>
+        
       ))}
     </div>
   );
