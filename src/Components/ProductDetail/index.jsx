cf18ddeb290dcb379c0eb81fd3d2f9fc3a7ca04b
Kevin Riquelme <kriquelme10@gmail.com>
2023-12-12 22:07:37 -0300

    CSS a app

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -34,15 +34,15 @@ export const ProductDetail = () => {
     return total.toFixed(2);
   };
 
-
   return (
     <div className="content-side-nav">
-      <div className="flex justify-between m-4">
+      <div className="flex justify-between m-2 p-2 border-b-[1px]">
         <span className="font-bold">My Order</span>
         <span
           className="cursor-pointer"
           onClick={() => context.toggleProductDetail()}
         >
+          {/* Icono para cerrar */}
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -63,17 +63,17 @@ export const ProductDetail = () => {
         {context.items.map((item) => (
           <div
             key={item.id}
-            className="flex mt-8 mb-4 p-4 justify-start items-center w-full"
+            className="flex mt-6 mb-2 p-2 justify-center items-center w-full "
           >
-            <div className="flex items-center justify-center w-[225px] pr-2">
-              <figure className="w-16 h-18">
+            <div className="flex items-center justify-center w-[150px] pr-2 grow">
+              <figure className="w-16 h-18 ">
                 <img
                   src={item.image}
                   alt=""
                   className="object-cover w-full h-full"
                 />
               </figure>
-              <p className="text-xs pl-2 mr-2 w-28">{item.title}</p>
+              <p className="text-xs pl-4 w-28">{item.title}</p>
             </div>
             <p className="text-sm  font-bold flex">
               <span>
@@ -81,8 +81,9 @@ export const ProductDetail = () => {
               </span>
               {item.price}
             </p>
+            {/* Icono para quitar el item */}
             <span
-              className="pl-2 cursor-pointer "
+              className="flex cursor-pointer  pl-2 justify-end w-auto"
               onClick={() => itemDeleted(item)}
             >
               <svg
@@ -103,12 +104,13 @@ export const ProductDetail = () => {
           </div>
         ))}
       </nav>
+      {/* Si hay elementos en el carrito, muestro el total */}
       {context.items.length > 0 && (
-        <p className="flex justify-center text-base items-center font-semibold  border-solid border-black bg-black border-[1px] w-[120px] h-[35px] rounded-lg text-white mt-auto mb-4 ml-auto mr-auto">
-          Total <IoLogoUsd style={{marginLeft: "8px"}}/>{totalCheckout()}
+        <p className="flex justify-center text-base items-center font-semibold  border-solid border-black bg-black border-[1px] w-[120px] h-[35px] rounded-lg text-white mt-auto  ml-auto mr-auto">
+          Total <IoLogoUsd style={{ marginLeft: "8px" }} />
+          {totalCheckout()}
         </p>
-        )
-      }
+      )}
       <button
         className="button p-4 m-2  border rounded-lg
        bg-black text-slate-50 font-bold"
